f922cc3a707ac297542ccb4ca40f70c1deab00db
Adam <ins1gn1a@users.noreply.github.com>
2018-06-04 16:32:30 +0100

    Create k-pop.py

---
+++ k-pop.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+
+import requests
+import argparse
+from hashlib import sha1
+from time import sleep
+
+parser = argparse.ArgumentParser(description='HaveIBeenPwned K-Anonymity Mass Checker')
+parser.add_argument('--pass-file',help="Path to file containing passwords",dest='pass_path')
+#parser.add_argument('--user-pass',help="Path to file containig 'username:password' to check")
+args = parser.parse_args()
+
+with open(args.pass_path) as passfile:
+    password_list = passfile.read().splitlines()
+
+for password in password_list:
+    hash = ((sha1(password.encode('utf-8'))).hexdigest()).upper()
+
+    url = ("https://api.pwnedpasswords.com/range/" + hash[0:5])
+    response = ((requests.get(url)).text).split("\r\n")
+
+    for line in response:
+
+        if len(line) > 1:
+            line_hash = line.split(":")[0]
+            line_count = line.split(":")[1]
+            if line_hash.rstrip() == hash.rstrip()[5:]:
+                print ("Password '" + password + "' has been owned " + line_count + " times.")
+				continue
+    # Rate limit
+    sleep(1.5)
